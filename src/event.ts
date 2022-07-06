3175e8921b88bcf207d7dd665cda933574aa9079
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-07-06 22:42:35 +0200

    feat(event): add DomainEvent and PublicDomainEvent

--- src/event.ts
+++ src/event.ts
@@ -1,3 +1,26 @@
 export class Event {
-  public timestamp: string; // ISO string YYYY-MM-DDTHH:mm:ss.sssZ with UTC offset
+  public payload: unknown;
+  public metadata: EventMetadata;
+  public isPublic: boolean;
+
+  constructor(isPublic: boolean) {
+    this.isPublic = isPublic
+  }
 }
+
+export class DomainEvent extends Event {
+  constructor() {
+    super(false)
+  }
+}
+
+export class PublicDomainEvent extends Event {
+  constructor() {
+    super(true)
+  }
+}
+
+export type EventMetadata = {
+  publishedAt: string  // ISO string YYYY-MM-DDTHH:mm:ss.sssZ with UTC offset
+}
+
