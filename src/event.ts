da2d8ce8cace0a98e71c3999d662b022feda6cee
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-10-03 11:56:58 +0200

    feat(event): add eventName

--- src/event.ts
+++ src/event.ts
@@ -1,28 +1,23 @@
 export class Event<PayloadType> {
+  public eventName: string;
   public payload: PayloadType;
-  public metadata: EventMetadata;
-  public isPublic: boolean;
   public aggregateId: string;
+  public isPublic: boolean;
 
   constructor(aggregateId: string, isPublic: boolean) {
-    this.aggregateId = aggregateId
-    this.isPublic = isPublic
+    this.aggregateId = aggregateId;
+    this.isPublic = isPublic;
   }
 }
 
 export class DomainEvent<PayloadType> extends Event<PayloadType> {
   constructor(aggregateId: string) {
-    super(aggregateId, false)
+    super(aggregateId, false);
   }
 }
 
 export class PublicDomainEvent<PayloadType> extends Event<PayloadType> {
   constructor(aggregateId: string) {
-    super(aggregateId, true)
+    super(aggregateId, true);
   }
 }
-
-export type EventMetadata = {
-  publishedAt: string  // ISO string YYYY-MM-DDTHH:mm:ss.sssZ with UTC offset
-}
-
