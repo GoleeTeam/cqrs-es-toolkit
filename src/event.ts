512845f9ef66c5c27ba6f7181d97e5e1b0078380
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-10-03 12:23:17 +0200

    refactor(event)

--- src/event.ts
+++ src/event.ts
@@ -1,15 +1,16 @@
 export abstract class Event<EventPayloadType> {
   abstract readonly eventName: string;
-  abstract readonly eventPayload: EventPayloadType;
   abstract readonly aggregateId: string;
+  abstract readonly eventPayload: EventPayloadType;
+  abstract readonly isPublic: boolean;
 }
 
 export class PrivateEvent<EventPayloadType> implements Event<EventPayloadType> {
   readonly isPublic = false;
 
   constructor(
-    readonly aggregateId: string,
     readonly eventName: string,
+    readonly aggregateId: string,
     readonly eventPayload: EventPayloadType
   ) {}
 }
@@ -17,8 +18,8 @@ export class PublicEvent<EventPayloadType> implements Event<EventPayloadType> {
   readonly isPublic = true;
 
   constructor(
-    readonly aggregateId: string,
     readonly eventName: string,
+    readonly aggregateId: string,
     readonly eventPayload: EventPayloadType
   ) {}
 }
